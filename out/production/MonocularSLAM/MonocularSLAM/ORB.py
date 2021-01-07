a3f4e37788f92010f435831fe44802e5b08f2bba
Heaper327 <jameszhu327@outlook.com>
2021-01-07 10:53:37 -0500

    Feat: remove javafx

--- out/production/MonocularSLAM/MonocularSLAM/ORB.py
+++
@@ -1,6 +0,0 @@
-import numpy
-import cv2
-
-image = cv2.imread("test.jpg", cv2.IMREAD_COLOR)
-cv2.imshow("test", image)
-
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+
+// runner class for simple testing
+public class Main {
+
+
+}
